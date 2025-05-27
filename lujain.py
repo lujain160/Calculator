79785cb6e15e08b8edab54e9efd7d912ac5b6025
lujain abdullah <seniseviyorumg7@gmail.com>
2025-05-27 12:00:21 +0300

    Update lujain.py

--- lujain.py
+++ lujain.py
@@ -1,10 +1,1 @@
-name=input('Enter your name: ')
-print(f'Welcom{name} to this adventure!')
-answer=input('You are on dirt road, it has come to an end and you can go left or right, Which way would you like to go?  ').lower()
-if answer=='left':
-    answer=input('You come to a river , you can walk around it or swim accross> Type (walk) to walk around and (swim) to swim around accross: ').lower()
-    
-elif answer=='right':
-    print('')
-else:
-    print('Not a valid option. You lose')
+
